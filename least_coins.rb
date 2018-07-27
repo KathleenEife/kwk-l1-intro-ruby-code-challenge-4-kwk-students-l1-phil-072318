eb9c7131ef07cfa1b6204e7d8e418a594bbdddc8
Kathleen Eife <keife218@icloud.com>
2018-07-27 13:13:31 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,12 @@
 #write out your code here
-
+puts "How many cents do you have?"
+cents = gets.chomp.to_i 
 def least_coins(cents)
-
-#Code your answer here!
+  
+  quarter_count = cents/25
+  dime_count = cents/10
+  nickels_count = cents/5
+  penny_count = cents/1  
 
 end
+return hash = {:quarters => quarter_count, :dimes => dime_count, :nickels => nickels_count, :pennies => penny_count}
